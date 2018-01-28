031c7c1ba7357fa89c0e86eab2f52a9484441d88
antonio.lima <antonio.meireles92@gmail.com>
2018-01-28 22:51:19 +0000

    Created a service to retrive data related with products

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
   selector: 'pm-products',
@@ -15,35 +16,11 @@ export class ProductListComponent implements OnInit{
     _filterText: string;
     ratingToHeader: string;
 
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-    ];
-
+    products: IProduct[] = [];
     filteredProducts: IProduct[];
 
-    constructor() {
-        this.filterText = '';
-        this.filteredProducts = this.products;
-    }
+    constructor(private _productService: ProductService) { }
+
     get filterText(): string {
         return this._filterText;
     }
@@ -64,7 +41,8 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        console.log("On init runned!")
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
     }
 
     onRaitingClicked(rating: number): void {
