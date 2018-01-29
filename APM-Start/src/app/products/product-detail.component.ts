bbfcd4eadadbae2b3ab9efde4d1c5679c5967f44
antonio.lima <antonio.meireles92@gmail.com>
2018-01-29 00:23:09 +0000

    Added product details component

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from "./product";
 
 @Component({
-  selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+    pageTitle: string;
+    product: IProduct;
 
-  constructor() { }
+    constructor() {
+        this.pageTitle = "Product Detail"
+    }
 
   ngOnInit() {
   }
